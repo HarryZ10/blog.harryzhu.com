b902f66d5a98c64d1ce94f6c3ae10e055521a940
Harry Zhu <harryzhu45@gmail.com>
2024-01-12 06:28:24 +0000

    Add allow origin uri

--- index.php
+++ index.php
@@ -3,6 +3,8 @@ require_once __DIR__.'/backend/routes/Router.php';
 
 header('Content-Type: application/json');
 header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Origin: http://10.10.10.25:3000");
+
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: GET,POST,PUT,DELETE,OPTIONS");
 header("Access-Control-Max-Age: 3600");
