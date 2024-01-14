12ddcca47fad27224cddece9b8c0cb7a45e7eeec
Harry Zhu <harryzhu45@gmail.com>
2024-01-14 21:09:31 +0000

    Remove broken import

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
-import PostPage from './pages/PostPage';
 import BlogPage from './pages/BlogPage';
 import { Logout } from './api/UsersAPI';
+
 /**
  * Manages routing on the client side to different pages
  */
