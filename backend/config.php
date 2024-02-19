e8e190ae1e986f3d4b2a454deec4eedbb0a5c143
Harry Zhu <harryzhu45@gmail.com>
2024-02-19 04:48:13 +0000

    Change from dev to prod

--- backend/config.php
+++ backend/config.php
@@ -4,16 +4,17 @@
     define('DB_PORT', '5432');
 
     // Local development
-    define('DB_NAME', 'program');
-    define('DB_USER', 'dev');
-    define('DB_PASSWORD', '314dev');
-    define('HTTP_ORIGIN', 'http://10.10.10.25:3000');
+    //define('DB_NAME', 'program');
+    //define('DB_USER', 'dev');
+    //define('DB_PASSWORD', '314dev');
+    //define('HTTP_ORIGIN', 'http://10.10.10.25:3000');
+    define('HTTP_ORIGIN', 'http://web.cs.georgefox.edu/~hzhu20');
 
     // Production mode
-    define('PRODUCTION_MODE', false);
+    define('PRODUCTION_MODE', true);
 
     // Production environment
-    // define('DB_NAME', 'blog');
-    // define('DB_USER', 'charlie');
-    // define('DB_PASSWORD', 'charlie');
+    define('DB_NAME', 'blog');
+    define('DB_USER', 'charlie');
+    define('DB_PASSWORD', 'charlie');
 ?>
