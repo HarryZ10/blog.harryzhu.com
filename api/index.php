6387fda37c5455350ab73700d586e9f442a75958
Harry <harryzhu45@gmail.com>
2024-05-15 17:43:41 -0700

    one more time

--- api/index.php
+++ api/index.php
@@ -9,11 +9,10 @@ $httpOrigin = 'https://blog.harryzhu.com';
 
 header('Content-Type: application/json');
 header("Access-Control-Allow-Origin: $httpOrigin");
-
-header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: GET,POST,PUT,DELETE,OPTIONS");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header("Access-Control-Allow-Credentials: true");
 
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     header('Access-Control-Allow-Origin: *');
