9dcea421e21c954c07c224560aee18ea6b2329bc
Harry <harryzhu45@gmail.com>
2024-11-01 21:57:18 -0700

    fix: cors fix

--- api/index.php
+++ api/index.php
@@ -5,24 +5,18 @@ error_reporting(E_ALL);
 ini_set('display_errors', '0');
 ini_set('log_errors', '1');
 
-$httpOrigin = 'https://blog.harryzhu.com';
-
-header('Content-Type: application/json; charset=UTF-8');
-header("Access-Control-Allow-Origin: $httpOrigin");
+// $httpOrigin = 'https://blog.harryzhu.com';
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET,POST,PUT,DELETE,OPTIONS");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 header("Access-Control-Allow-Credentials: true");
 
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    header('Access-Control-Allow-Origin: *');
-    header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, OPTIONS');
-    header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
     http_response_code(200);
     exit();
 }
 
 $router = new Router();
 $router->handleRequest();
-
 ?>
